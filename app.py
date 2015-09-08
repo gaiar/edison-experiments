13cab9d024019be62580a94908e1492bf64b7bc3
Gaiar <gaiar@baimuratov.ru>
2015-09-08 21:33:50 +0300

    First try

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ __author__ = 'gaiar'
 from sensors import Sensors
 
 
-print(Sensors.get_light_sensor_data())
-print(Sensors.get_uv_sensor_data())
+print (Sensors().get_uv_sensor_data())
+print (Sensors().get_light_sensor_data())
