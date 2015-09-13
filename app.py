f36b6f9080c788084d3bb48c45ea42de908168de
Gaiar <gaiar@baimuratov.ru>
2015-09-13 17:54:29 +0300

    Added telegram module

--- app.py
+++ app.py
@@ -1,40 +1,167 @@
 __author__ = 'gaiar'
 
+import time, sys, signal, atexit
 from sensors import Sensors
 import telegram
-import pyupm_i2clcd as lcd_screen
+import pyupm_rotaryencoder as upm_rotary_encoder
 
-#print('UV: ' + str(Sensors().get_uv_sensor_data()))
-print('Light: ' + str(Sensors().get_light_sensor_data()))
-print('Humidity: ' + str(Sensors().get_humidity_sensor_data()))
-print('Temperature: ' + str(Sensors().get_temp_sensor_data()))
-Sensors().display_ths_sensor_data()
 
-lcd = lcd_screen.Jhd1313m1(0, 0x3E, 0x62)
-lcd.write('Hi, Gaiar!')
+import operator
 
-lcd.clear()
-lcd.write(" Temp&Humidity  ")
-#lcd.write("<-")
-lcd.setCursor(1, 1)
-print (str(Sensors().get_temp_sensor_data()))
-lcd.write(str(Sensors().get_temp_sensor_data()))
-lcd.write("C ")
+global mysensors
+mysensors = Sensors()
 
-# sprintf(number,"%d",getHumiSensorValue());
-lcd.setCursor(1, 9)
-lcd.write(str(Sensors().get_humidity_sensor_data()))
-lcd.write("%")
-# lcd.setCursor(numCols-2,1);
-# lcd.write("->");
+CMDSTR_MAX_LEN = 128
+LOW_TEMP = 10
+NOM_TEMP = 25
+HIGH_TEMP = 40
 
-# display_ths_sensor_data()
+SENSOR_COUNT = 6
+ACTUATOR_COUNT = 4
+VAR1_COUNT = SENSOR_COUNT
+VAR2_COUNT = ACTUATOR_COUNT + 1
+isBackLightOn = True
+cmdstr = ''
 
-raw_input("Press Enter if you believe...")
+BLColorRGB = {0x00, 0x00, 0xFF}
+
+numRows = 2
+numCols = 16
+
+MenuCount = 5
+MenuIndex = 0
+
+getSensorValueList = [
+    {'sensor': 'temperature', 'value': Sensors().get_temp_sensor_data},
+    {'sensor': 'humidity', 'value': Sensors().get_humidity_sensor_data},
+    {'sensor': 'light', 'value': Sensors().get_light_sensor_data},
+    {'sensor': 'uv', 'value': Sensors().get_uv_sensor_data},
+    {'sensor': 'encoder', 'value': Sensors().get_encoder_data},
+    {'sensor': 'moisture', 'value': Sensors().get_moisture_sensor_data}]
+
+ops = {
+    ">": operator.ge,
+    "<": operator.lt,
+    "=": operator.eq
+}
+
+
+def servo_handle(value):
+    curt = current_milli_time
+    if (current_milli_time - curt > 1000):
+        #servo.setAngle(value)
+        curt = current_milli_time
+
+
+def sleep_handle():
+    return
+
+
+def relay_handle():
+    return
+
+
+def buzzer_handle():
+    return
+
+
+SensorValue = []
+
+ActuatorHandlerList = [relay_handle, buzzer_handle, servo_handle, sleep_handle]
+
+# value, condition, Actuator, action
+SensorConfig = [
+    {'sensor': 'Temperature', 'value': 85, 'condition': '>', 'handler': sleep_handle, 'trigger': 1},
+    {'sensor': 'Humidity', 'value': 100, 'condition': '>', 'handler': sleep_handle, 'trigger': 1},
+    {'sensor': 'Light', 'value': 200, 'condition': '<', 'handler': servo_handle, 'trigger': 90},
+    {'sensor': 'UV', 'value': 1023, 'condition': '>', 'handler': sleep_handle, 'trigger': 1},
+    {'sensor': 'PIR', 'value': 2, 'condition': '=', 'handler': buzzer_handle, 'trigger': 1},
+    {'sensor': 'Moisture', 'value': 600, 'condition': '<', 'handler': relay_handle, 'trigger': 1}
+]
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+## Exit handlers ##
+# This function stops python from printing a stacktrace when you hit control-C
+def SIGINTHandler(signum, frame):
+    raise SystemExit
+
+
+def exitHandler():
+    print("Exiting")
+    sys.exit(0)
+
+
+def print_settings():
+    print('The Sensors Configurations as follow:')
+    i = 0
+    for i in range(0, SENSOR_COUNT):
+        print(SensorConfig[i]['sensor'] + " " + SensorConfig[i]['condition']) + " " + str(SensorConfig[i]['value'])
+
+        i += 1
+    print("\n")
+    print("The Sensors Value as follow:")
+    i = 0
+    for i in range(0, SENSOR_COUNT):
+        print(getSensorValueList[i]['sensor'] + ' = ' + str(getSensorValueList[i]['value']()))
+        i += 1
+    print("\n")
+
+
+def serial_request_handler():
+    changed_value = 0
+    i = 0
+
+    for i in range(0, SENSOR_COUNT):
+        if (SensorConfig[i][3] == servo_handle):
+            changed_value = 0
+        else:
+            changed_value = not SensorConfig[i][1]
+        if (ops[SensorConfig[i][2]](getSensorValueList[i], SensorConfig[i][1])):
+            SensorConfig[i][3](SensorConfig[i][4])
+        else:
+            SensorConfig[i][3](changed_value)
+        i += 1
+
+
+def is_button_pressed():
+    return button.value()
+
+
+# def relay_handle(state):
+
+# atexit.register(exitHandler)
+# signal.signal(signal.SIGINT, SIGINTHandler)
+
+last_data = None
+print_settings()
+
+
+
+
+def BuzzerHandler(val):
+    return
+
+
+def temp_color_handler():
+    return
+
+
+def display_menu():
+    return
 
 
 def get_menu_index():
-    
+    return ''
+
 
 def display_menu():
     return ''
+
+
+def app(self):
+    temp_color_handler()
+    display_menu()
+    if (button.value()):
+        self.isBackLightOn = not self.isBackLightOn
+    return
--- sensors.py
+++ sensors.py
@@ -1,7 +1,11 @@
-import pyupm_i2clcd as lcd_screen
+import pyupm_i2clcd as upm_lcd
 import pyupm_grove as grove
 import pyupm_th02 as ths
 import pyupm_rotaryencoder as rotary_encoder
+import pyupm_guvas12d as upm_uv
+import pyupm_buzzer as upm_buzzer
+import pyupm_servo as upm_servo
+
 import mraa
 import time
 import math
@@ -10,51 +14,92 @@ import math
 class Sensors(object):
     SensorsData = {}
 
-    PIN_BUZZER = 4
+    PIN_BUZZER = 6
     PIN_LIGHT = 2
     PIN_UV = 3
     PIN_LED = 3
-    PIN_ENCODER = 2
+    PIN_ENCODER1 = 2
+    PIN_ENCODER2 = 3
     PIN_BUTTON = 1
     PIN_SOUND = 7
     PIN_RELAY = 5
+
+    GUVAS12D_AREF = 5.0
+    SAMPLES_PER_QUERY = 1024
+
+    encoder = rotary_encoder.RotaryEncoder(PIN_ENCODER1, PIN_ENCODER2)
+    light_sensor = grove.GroveLight(PIN_LIGHT)
+    uv = upm_uv.GUVAS12D(PIN_UV)
+    th02 = ths.TH02()
+    lcd = upm_lcd.Jhd1313m1(0, 0x3E, 0x62)
+    buzzer = upm_buzzer.Buzzer(PIN_BUZZER)
+    relay = grove.GroveRelay(0)
+    button = grove.GroveButton(8)
+    servo = upm_servo.Servo(5)
+
+    def __init__(self):
+        self.encoder.initPosition(0)
+        self.buzzer.stopSound()
 
     def get_moisture_sensor_data(self):
         return ''
 
     def get_light_sensor_data(self):
-        light_sensor = grove.GroveLight(self.PIN_LIGHT)
-        light = light_sensor.value()
+        light = self.light_sensor.value()
         self.SensorsData['light'] = light
         return light
 
     def get_uv_sensor_data(self):
-        sensor_value = 0
-        sensor_data_sum = 0
-        uv = mraa.Aio(self.PIN_UV)
-        i = 0
-        for i in range(0, 1024):
-            sensor_value = uv.read()
-            sensor_data_sum += sensor_value
-            time.sleep(0.002)
-            i += 1
-        sensor_data_sum = sensor_data_sum >> 10
-        self.SensorsData['uv'] = float(sensor_data_sum * 4980.0 / 1023.0)
-        # return float(sensor_data_sum * 4980.0 / 1023.0)
+        sensor_value = self.uv.value(self.GUVAS12D_AREF, self.SAMPLES_PER_QUERY)
+        self.SensorsData['uv'] = sensor_value
         return sensor_value
 
     def get_encoder_data(self):
-        encoder = rotary_encoder.RotaryEncoder(self.PIN_ENCODER)
-        return encoder.position()
+        return self.encoder.position()
 
     def get_temp_sensor_data(self):
-        th02 = ths.TH02()
-        temp = th02.getTemperature()
+        temp = self.th02.getTemperature()
         self.SensorsData['temp'] = temp
         return int(temp)
 
     def get_humidity_sensor_data(self):
-        th02 = ths.TH02()
-        temp = th02.getHumidity()
+        temp = self.th02.getHumidity()
         self.SensorsData['humidity'] = temp
         return int(temp)
+
+    def show_temp_and_humidity(self):
+        self.lcd.clear()
+        self.lcd.write(" Temp&Humidity  ")
+        # lcd.write("<-")
+        self.lcd.setCursor(1, 1)
+        self.lcd.write(str(Sensors().get_temp_sensor_data()) + "C ")
+        self.lcd.setCursor(1, 9)
+        self.lcd.write(str(Sensors().get_humidity_sensor_data()) + "%")
+
+    def show_light_and_uv(self):
+        self.lcd.clear()
+        self.lcd.write(" Light&UV  ")
+        # lcd.write("<-")
+        self.lcd.setCursor(1, 1)
+        self.lcd.write(str(Sensors().get_light_sensor_data()))
+        self.lcd.setCursor(1, 9)
+        self.lcd.write(str(Sensors().get_uv_sensor_data()))
+
+    def set_buzzer(self, value):
+        self.buzzer.playSound(upm_buzzer.SI, 100000)
+
+    def turn_off(self):
+        del self.encoder
+        del self.lcd
+        del self.uv
+        del self.light_sensor
+        del self.th02
+
+    def __str__(self):
+        sensors_value = "====================\n"
+        sensors_value += "Humidity: " + str(self.get_humidity_sensor_data()) + "% " + "\n"
+        sensors_value += "Temperature: " + str(self.get_temp_sensor_data()) + "C " + "\n"
+        sensors_value += "UV: " + str(self.get_uv_sensor_data()) + "\n"
+        sensors_value += "Light: " + str(self.get_light_sensor_data()) + "\n"
+        sensors_value += "===================="
+        return sensors_value
